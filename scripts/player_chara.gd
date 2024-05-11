a487d77d9eaa7fcb4f722bff022b42769f02c483
mharde <maggiehawrites@gmail.com>
2024-05-11 16:00:12 -0400

    Reorganized folders and flipped character sprite

--- scripts/player_chara.gd
+++ scripts/player_chara.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,6 +20,14 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	#Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	if direction < 0:
+		animated_sprite.flip_h = true
+	
+	#Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -27,7 +35,3 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-
-func _input(event):
-	if Input.is_action_just_pressed("interact"):
-		load.dialog
