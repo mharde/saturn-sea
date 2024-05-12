3859f1c86751d0cefc585cba7a9bf595cfc22c5b
mharde <maggiehawrites@gmail.com>
2024-05-12 14:32:11 -0400

    Added parallax backgrounds

--- scripts/player_chara.gd
+++ scripts/player_chara.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 120.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,7 +18,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	#Flip the Sprite
