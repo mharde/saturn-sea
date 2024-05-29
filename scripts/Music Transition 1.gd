68f494d8657a5ba2a4fe24a57fbc04914c8c049a
Jamail Chachere <jamailchachere@gmail.com>
2024-05-28 19:56:27 -0500

    Music added in streamplayer
    Strings stem and Core loop on auto play. Find elder not autostart

---
+++ scripts/Music Transition 1.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+#Call the function "on body exit"
+
+#@onready var Strings: AudioStreamPlayer = $"../Strings stem"
+#func _on_body_entered(body):
+	#if body.is_in_group("Player"):
+		#Strings.stop()
+
+		
+		
+ #void play(float from_position=0.0)
---
+++ scripts/Music Transition 2.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+#@onready var Strings: AudioStreamPlayer = $"../Strings stem"
+#func _on_body_entered(body):
+	#if body.is_in_group("Player"):
+		#Strings.stop()
